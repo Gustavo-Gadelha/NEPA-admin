3a8226a48c8864ce7c0323c96b6c13ce7fc3d478
Gustavo-Gadelha <gustavofg2112@gmail.com>
2026-03-10 21:07:53 -0300

    feat: página de erros e separação de handlers em um arquivo separado

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask
 
 load_dotenv()
 
@@ -8,20 +8,15 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config")
 
-    @app.errorhandler(404)
-    def not_found(error):
-        return render_template("404.html"), 404
-
     from app.extensions import admin, db, login_manager, ph
     from app.views import SecureAdminIndexView
 
     db.init_app(app)
+    admin.init_app(app, index_view=SecureAdminIndexView())
     ph.init_app(app)
     login_manager.init_app(app)
 
-    admin.init_app(app, index_view=SecureAdminIndexView())
-
-    from app.models import Aluno, AlunoProjeto, Edital, Professor, Projeto
+    from app.models import Admin, Aluno, AlunoProjeto, Edital, Professor, Projeto
     from app.views import AlunoAdmin, AlunoProjetoAdmin, EditalAdmin, ProfessorAdmin, ProjetoAdmin
 
     admin.add_view(AlunoAdmin(Aluno, db.session))
@@ -30,13 +25,16 @@ def create_app():
     admin.add_view(ProfessorAdmin(Professor, db.session))
     admin.add_view(ProjetoAdmin(Projeto, db.session))
 
-    from app.models import Admin
-    from app.routes import bp
-
-    app.register_blueprint(bp)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.get(Admin, user_id)
 
     from app.commands import register_commands
 
     register_commands(app)
+
+    from app.exceptions import register_error_handlers
+
+    register_error_handlers(app)
 
     return app
--- app/extensions.py
+++ app/extensions.py
@@ -1,17 +1,10 @@
 from flask_admin import Admin
+from flask_admin.theme import Bootstrap4Theme
 from flask_argon2 import Argon2
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-admin = Admin()
+admin = Admin(theme=Bootstrap4Theme(base_template="master.html", fluid=True))
 ph = Argon2()
 login_manager = LoginManager()
-login_manager.login_view = "auth.login"
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    from app.models import Admin
-
-    return Admin.query.get(user_id)
